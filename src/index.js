49b80ba963f01436e803072cec8a0e0b265908bc
Vitaliy Semenov <synthex@bk.ru>
2023-01-02 22:26:48 +0600

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = n.toString().split('');
+  let length = n.length;
+  let res = [];
+  for (let i = length - 1; i >= 0; i--) {
+    res.push(n[i]);
+  }
+  return +res.join('');
 }
